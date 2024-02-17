c718984c77a34807918d81fdf3a8a5203012f28b
Hayden Lister <haydenglister@gmail.com>
2024-02-17 12:49:22 -0800

    Created initial game window

---
+++ mazeSolver.py
@@ -0,0 +1,27 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.geometry(f"{width}x{height}")
+        self.canvas = Canvas(self.__root)
+        self.canvas.pack()
+        self.running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running == True:
+            self.redraw()
+        
+    def close(self):
+        self.running = False
+
+if __name__ == "__main__":
+    win = Window(800, 600)
+    win.wait_for_close()
